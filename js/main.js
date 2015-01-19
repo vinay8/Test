9457fffde54744108928def5ab8bcca6e603f2b0
vinay8 <vinaysharma2@savinay.local>
2015-01-19 16:06:24 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var app = {
+
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
+
+    initialize: function() {
+        this.store = new MemoryStore();
+//        this.store = new LocalStorageStore();
+//        this.store = new WebSqlStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+},
+
+};
+
+app.initialize();
